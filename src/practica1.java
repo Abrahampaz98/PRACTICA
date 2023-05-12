ad9a4f0a99b379e867be83b1d708d7380aa114ee
AP98 <AP98@DESKTOP-LLLPGM8>
2023-05-12 17:40:51 -0500

    carga inicial

--- src/practica1.java
+++ src/practica1.java
@@ -1,8 +1,13 @@
-public class practica1 {
+ class practica1{
     public static void main(String[] args) {
-        final byte n=12; //es para establecere una constante.
-        final float N = 0.12f; //es para establecere una constante.
-        System.out.println(n);
-        System.out.println(N);
-    }   
+        byte n=12;
+        float n2 = 0.12f; //
+        short n3=200;
+        int n4= 1300;
+        double n5= 22.3;
+        String l="esto es un cadena de caracteres";
+        char l2='a';
+        System.out.println("estos son los tipos de datos: \n"+n+"\n"+n2+"\n"+n3+"\n"+n4+"\n"+n5+"\n"+l+"\n"+l2); 
+    }
+    
 }
